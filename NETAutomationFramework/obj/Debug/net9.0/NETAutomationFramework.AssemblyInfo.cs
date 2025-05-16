9dc5be7c01ac2f82d42de7ffb821838cd4d965ac
Kiet Do <tuankiet141199@gmail.com>
2025-05-17 00:40:17 +0700

    feat: initial commit with .NET automation framework
    - Set up basic framework structure with POM pattern
    - Added StyleCop for code analysis
    - Configured test reporting and screenshot capture
    - Added CI/CD pipeline with GitHub Actions

--- NETAutomationFramework/obj/Debug/net9.0/NETAutomationFramework.AssemblyInfo.cs
+++ NETAutomationFramework/obj/Debug/net9.0/NETAutomationFramework.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NETAutomationFramework")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2fc3a52e8fa8da17254b5855ae7b88c3333851fa")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+31530c81de82a16e6e3ed792637b80b9b5002576")]
 [assembly: System.Reflection.AssemblyProductAttribute("NETAutomationFramework")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NETAutomationFramework")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
